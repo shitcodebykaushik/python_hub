8b8573f22edfcf8e807d2abe23ba049fdb87b424
Kaushik raj <algorithmunloack@gmail.com>
2024-08-20 15:40:55 +0530

    The project is ready to ship

---
+++ mini_project/03.py
@@ -0,0 +1,7 @@
+while True:
+    print("Program is running...")
+    user_input = input("Enter 'stop' to end the loop: ")
+
+    if user_input == 'stop':
+        print("Stopping the loop.")
+        break
